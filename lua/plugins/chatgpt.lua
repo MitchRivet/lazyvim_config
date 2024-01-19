2234722128fbe611da109ffd3f1579ff713b3bb9
Mitchell Rivet <mitchellrivet@MitchelacStudio.lan>
2024-01-19 12:27:53 -0800

    add chatgpt plugin

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,14 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup({
+      api_key_cmd = "gpg --decrypt /PATH/TO/SECRET",
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}
